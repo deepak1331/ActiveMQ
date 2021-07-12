896f962f40ca2bab6fee5e8997a164756a41b7c4
Deepak Yadav <Deepak@192.168.56.1>
2021-07-13 03:35:10 +0530

    ActiveMq standalone demo commit

---
+++ src/test/java/com/deepak/learn/ActiveMQ/Demo/ActiveMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.learn.ActiveMQ.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
